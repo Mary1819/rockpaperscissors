e1d5af96cdcf94f2d30e0297d5fe5b4e7cd3e9f9
Glitch (rock-paper-scissors-groupproject) <none>
2021-12-09 00:44:42 +0000

    🍫 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,1 +1,45 @@
+$(".choices").hide();
+
+
+  
+  
+   let computerOutput= Math.ceil(Math.random() * 3);
+  // write the numbers as rock paper scissors
+  if(computerOutput === 1) {
+    let computerInput = "rock;"
+    (".computerChoice").text("rock");
+} else if(computerOutput === 2) {
+      let computerInput = "paper;"
+    (".computerChoice").text("paper");
+} else if (computerOutput === 3) {
+      let computerInput = "sicssor;"
+  (".computerChoice").text("scissor")
+}
+ 
+ $(".shoot").click(function () {
+   let userInput= $(".input").val();
+   let computerRock= "rock";
+   let computerPaper= "paper";
+   let computerScissor= "scissor"
+   
+   $(".choices").show();
+   $(".userChoice").text(userInput);
+   
+   // if( userInput === computerRock ){
+    if(userInput === computerOutput){
+       // computerRock || userInput === computerPaper || userInput === computerScissor) {
+     $("body").css("background-color","green");
+     $(".result").text("It's a tie!!!");
+     
+     
+     
+   } else{
+          $("body").css("background-color","red");
+          $(".result").text("You win!!!");
+
+   }
+ })
+  
+
+
 
