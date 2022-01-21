c1876673767af25b69804b0ad49a2f90ac1c3d37
Glitch (busy-piquant-countess) <none>
2022-01-21 00:44:38 +0000

    🎵🍄 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -1,98 +1,72 @@
 $(".computerChoicet").hide();
+let computerChoice;
+let userChoice;
+let winner;
+let won = 0;
+let lost = 0;
+let tie = 0;
 
-let result;
-let won;
-let lost;
-let tie;
-
-function getRandomComputerChoice(){
-  let computerChoice;
-  let computerOutput= Math.ceil(Math.random() * 3);
-    if(computerOutput === 1) {
-       computerChoice = "rock";
-       $(".computerChoicet").text("Rock");
-    } else if(computerOutput === 2) {
-       computerChoice = "paper";
-       $(".computerChoicet").text("Paper");
-    } else if (computerOutput === 3) {
-       computerChoice = "scissor";
-      $(".computerChoicet").text("Scissor");
-    }
-  return computerChoice;
+function getRandomComputerChoice() {
+  let computerOutput = Math.ceil(Math.random() * 3);
+  if (computerOutput === 1) {
+    computerChoice = "rock";
+  } else if (computerOutput === 2) {
+    computerChoice = "paper";
+  } else if (computerOutput === 3) {
+    computerChoice = "scissor";
+  }
+  $(".computerChoicet").text(computerChoice);
 }
 
-function chooseWinner(userChoice, computerChoice){
-  let won= 0;
-  let lost= 0;
-  let tie= 0;
-  
-    // tie = tie + 1;
-    // $(".resultt").text("It is a tie");
-    // $(".tie").text(tie);
-  
-    if(userChoice.toLowerCase() === computerChoice.toLowerCase()){}
-  
+function win() {
+  won = won + 1;
+  $(".won").text(won);
+  winner = "You win";
+}
+
+function tief() {
+  tie = tie + 1;
+  $(".tie").text(tie);
+  winner = "It's a tie";
+}
+
+function loss() {
+  lost = lost + 1;
+  $(".lost").text(lost);
+  winner = "You loss to a computer HAHAHA";
+}
+
+function chooseWinner() {
   if (userChoice === "rock" && computerChoice === "rock") {
-    tie = tie + 1;
-    $(".resultt").text("It is a tie");
-    $(".tie").text(tie);
-    
+    tief();
   } else if (userChoice === "paper" && computerChoice === "rock") {
-       won= won + 1;
-    (".resultt").text("You win");
-    $(".won").text(won);  
-    
+    win();
   } else if (userChoice === "scissor" && computerChoice === "rock") {
-    lost= lost + 1;
-   $(".resultt").text("You loss to a computer HAHAHA");
-  $(".lost").text(lost);  
-    
+    loss();
   } else if (userChoice === "rock" && computerChoice === "paper") {
- lost= lost + 1;
-    $(".resultt").text("You loss to a computer HAHAHA");
-    $(".lost").text(lost); 
-    
+    loss();
   } else if (userChoice === "paper" && computerChoice === "paper") {
-    tie = tie + 1;
-    $(".resultt").text("It is a tie");
-    $(".tie").text(tie);    
-    
+    tief();
   } else if (userChoice === "scissor" && computerChoice === "paper") {
-    won= won + 1;    
-    $(".resultt").text("You win");
-    $(".won").text(won);
-    
+    win();
   } else if (userChoice === "rock" && computerChoice === "scissor") {
-    won= won + 1;
-    $(".resultt").text("You win");
-    $(".won").text(won);
-    
- } else if (userChoice === "paper" && computerChoice === "scissor") {
-     lost= lost + 1;
-    $(".resultt").text("You loss to a computer HAHAHA");
-    $(".lost").text(lost); 
- } else if (userChoice === "scissor" && computerChoice === "scissor") {
-     tie = tie + 1;
-    $(".resultt").text("It is a tie");
-    $(".tie").text(tie);
-  
- } else {
-        $(".resultt").text("Not valid input. Check your spelling."); }
-}
- 
- $(".shoot").click(function () {
-     $(".computerChoicet").show(); 
-     let computerChoice= getRandomComputerChoice();
-     let userChoice= $(".input").val(); 
-     let winner= chooseWinner(userChoice, computerChoice);
-     $(".userChoicet").text(userChoice);
-       tie = tie + 1;
-          won= won + 1;
-       lost= lost + 1;
+    win();
+  } else if (userChoice === "paper" && computerChoice === "scissor") {
+    loss();
+  } else if (userChoice === "scissor" && computerChoice === "scissor") {
+    tief();
+  } else {
+    winner = "Not valid input. Check your spelling.";
+  } }
 
-
-
-
+$(".shoot").click(function () {
+  getRandomComputerChoice();
+  $(".computerChoicet").show();
+  userChoice = $(".input").val().toLowerCase();
+  $(".userChoicet").text(userChoice);
+  chooseWinner();
+  tie = tie + 1;
+  won = won + 1;
+  lost = lost + 1;
+  $(".resultt").text(winner);
 });
-
-
